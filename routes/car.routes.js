6084aef01d498cc52004ce63de3abc982f90ee5c
Julian Bajo <jbajopinedo@gmail.com>
2020-04-28 15:19:51 +0200

    pause add location & pictures

--- routes/car.routes.js
+++ routes/car.routes.js
@@ -1,71 +1,97 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const cloudUploader = require("../configs/cloudinary.config");
 
-const Car = require('../models/car.model')
-const User = require('../models/user.model')
+const Car = require("../models/car.model");
+const User = require("../models/user.model");
 
 // Read documents(list)
 
-router.get('/', (req, res, next) => {
-
-    Car.find()
-        .populate('creatorId')
-        .then(allCars => res.render('cars/cars-list', { allCars }))
-        .catch(err => console.log("Ha habido un error!", err))
-})
+router.get("/", (req, res, next) => {
+  Car.find()
+    .populate("creatorId")
+    .then((allCars) => res.render("cars/cars-list", { allCars }))
+    .catch((err) => console.log("Ha habido un error!", err));
+});
 
 // Read documents (details)
 
-router.get('/:carId/details', (req, res, next) => {
-
-    Car.findById(req.params.carId)
-        .populate('creatorId')
-        .then(car => res.render('cars/cars-detail', car))
-        .catch(err => console.log("Ha habido un error!", err))
-})
+router.get("/:carId/details", (req, res, next) => {
+  Car.findById(req.params.carId)
+    .populate("creatorId")
+    .then((car) => res.render("cars/cars-detail", car))
+    .catch((err) => console.log("Ha habido un error!", err));
+});
 
 // Create documents
 
-router.get('/create', (req, res, next) => {
-    Car.find()
-        .then(allCars => res.render('cars/cars-create', {allCars}))
-        .catch(err => next(new Error(err)))
-})
+router.get("/create", (req, res, next) => {
+  Car.find()
+    .then((allCars) => res.render("cars/cars-create", { allCars }))
+    .catch((err) => next(new Error(err)));
+});
 
-router.post('/create', (req, res, next) => {
+router.post(
+  "/create",
+  /*cloudUploader.single("pepe"),*/ (req, res, next) => {
+    console.log("hola");
 
-    const { brand, model, manufacturingYear, plate, description, kilometres, state, price, adStatus } = req.body
+    const location = {
+      type: "Point",
+      coordinates: [req.body.latitud, req.body.longitud],
+    };
 
-    Car.create({ brand, model, manufacturingYear, plate, description, kilometres, state, price, adStatus })
-        .then(() => res.redirect('/cars'))
-        .catch(err => console.log("Hubo un error", err))
-})
+    const newCar = new Car({
+      brand: req.body.brand,
+      model: req.body.model,
+      carImagePath: req.file.url,
+      manufacturingYear: req.body.manufacturingYear,
+      plate: req.body.plate,
+      description: req.body.description,
+      state: req.body.state,
+      kilometres: req.body.kilometres,
+      location,
+      price: req.body.price,
+      adStatus: req.body.adStatus,
+    });
+    console.log(req.body);
+
+    newCar.save((err) => {
+      if (err) {
+        next(err);
+      } else {
+        res.redirect("/cars");
+      }
+    });
+  }
+);
 
 // Edit documents
-router.get('/:car_id/edit', (req, res, next) => {
+router.get("/:car_id/edit", (req, res, next) => {
+  Car.findById(req.params.car_id)
+    .then((CarToEdit) => res.render("cars/cars-edit", CarToEdit))
+    .catch((err) => console.log("Hubo un error", err));
+});
 
-    Car.findById(req.params.car_id)
-        .then(CarToEdit => res.render('cars/cars-edit', CarToEdit))
-        .catch(err => console.log("Hubo un error", err))
-})
+router.post("/:car_id/edit", (req, res, next) => {
+    
+    const location = { type: 'Point', coordinates: [req.body.latitud, req.body.longitud] }
+    const { brand, model, manufacturingYear, plate, description } = req.body;
 
-router.post('/:car_id/edit', (req, res, next) => {
-
-    const { brand, model, manufacturingYear, plate, description } = req.body
-
-    Car.findByIdAndUpdate(req.params.car_id, { brand, model, manufacturingYear, plate, description }, { new: true })
-        .then(editedCar => res.redirect(`/cars/${editedCar._id}/details`))
-        .catch(err => console.log("Hubo un error", err))
-})
+  Car.findByIdAndUpdate(
+    req.params.car_id,
+    { brand, model, manufacturingYear, plate, description, location},
+    { new: true }
+  )
+    .then((editedCar) => res.redirect(`/cars/${editedCar._id}/details`))
+    .catch((err) => console.log("Hubo un error", err));
+});
 
 //Delete document
-router.post('/:car_id/delete', (req, res, next) => {
+router.post("/:car_id/delete", (req, res, next) => {
+  Car.findByIdAndRemove(req.params.car_id)
+    .then(res.redirect("/cars"))
+    .catch((err) => console.log(`Error  ${err}`));
+});
 
-    Car.findByIdAndRemove(req.params.car_id)
-      .then(res.redirect('/cars'))
-      .catch(err => console.log(`Error  ${err}`))
-})
-
-
-
-module.exports = router
+module.exports = router;
