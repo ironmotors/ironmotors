5286327714de9870e9d6be08b4dd80488835ad9f
Julian Bajo <57926970+JotaBp@users.noreply.github.com>
2020-04-28 18:44:03 +0200

    Update nodemailer.config.js
    Change email & password

--- configs/nodemailer.config.js
+++ configs/nodemailer.config.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer')
 let transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-        user: 'shumoreniko86@gmail.com',
-        pass: 'C@vallo94'
+        user: /*'yourEmail'*/,
+        pass: /*'Password'*/
     }
 })
 
